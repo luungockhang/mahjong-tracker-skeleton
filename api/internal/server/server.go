5b41d124599ec474df35d4ad4c8466b54ce9d11e
Monkey King <45637966+luungockhang@users.noreply.github.com>
2025-09-18 19:11:41 +0700

    changed package name

--- api/internal/server/server.go
+++ api/internal/server/server.go
@@ -3,7 +3,7 @@ package server
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/luungockhang/mahjong-tracker-skeleton/internal/players"
+	"github.com/luungockhang/mahjong-tracker-skeleton/internal/player"
 )
 
 type Server struct {
@@ -17,7 +17,7 @@ func NewServer(db *pgxpool.Pool) *Server {
 func (s *Server) RegisterRoutes(r chi.Router) {
 	r.Route("/api", func(r chi.Router) {
 		r.Route("/v1", func(r chi.Router) {
-			r.Mount("/players", players.Route(s))
+			r.Mount("/player", player.Route(s))
 			// auth routes later
 		})
 	})
